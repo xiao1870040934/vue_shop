856537fb53d5b39a38024754da7d42f0a4693911
xiaoge520 <1870040934@qq.com>
2022-04-10 17:55:26 +0800

    完成登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,18 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    "space-before-function-paren": 0,
+    "no-unused-vars": [2, {  
+      "vars": "local",
+      "args": "none" 
+    }],
+    "semi": [0],
+    "no-multiple-empty-lines": [0, {"max": 100}],
+    "no-mixed-spaces-and-tabs": [0],
+    "no-multi-spaces": 0,
+    "indent":[1,2],
   }
 }
